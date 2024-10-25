cc684422730c8ae9349e2ca06443d9ee6e728c00
SruthiPrasannakumar <sruthipras93@gmail.com>
2024-10-26 00:08:04 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+
+// import './App.css'
+
+// function App() {
+
+//   return (
+//     <>
+//      <h1>Background color changing App</h1>
+//     </>
+//   )
+// }
+
+// export default App
+
+
+
+
+function App() {
+  const handleClickFunction = (e) => {
+    const body = document.querySelector("body");
+    body.style.background = getRandomColor();
+    e.target.style.backgroundColor = getRandomColor();
+  };
+
+  function getRandomColor() {
+    let letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+  return (
+    <>
+      <div className="container">
+        <h1>Random Background Color Changer</h1>
+        <button className="btn" onClick={handleClickFunction}>
+          Click Me
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default App;
